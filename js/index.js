0b5168f2e243d963c03208c4613d25732a7b4984
sherifHelmy <helmysherif792@gmail.com>
2021-02-19 14:27:48 +0200

    navbars

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(".search1").click(function(){
+    $(".search-form1").slideToggle(500);
+})
+$(".search-form1 .fa-times").click(function(){
+    $(".search-box form").fadeOut(500);
+})
+$(".search2").click(function(){
+    $(".search-form2").slideToggle(500);
+})
+$(".search-form2 .fa-times").click(function(){
+    $(".search-box form").fadeOut(500);
+})
